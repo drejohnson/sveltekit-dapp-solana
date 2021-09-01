3478fbeeb07375ce59086729da896420855c55e1
drejohnson <dre@phreshr.com>
2021-09-01 09:01:59 +0800

    Added global helpers for solona web3

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,3 +1,19 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		import('buffer').then((Buffer) => {
+			global.Buffer = Buffer.Buffer;
+		});
+	});
+</script>
+
+<svelte:head>
+	<script>
+		global = globalThis; // for solana web3 repo
+	</script>
+</svelte:head>
+
 <slot />
 
 <style lang="postcss" global>
